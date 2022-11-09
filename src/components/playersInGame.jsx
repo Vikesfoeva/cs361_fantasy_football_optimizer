b06323d3f3530992282b8f4459343378e78b1b7a
BrandonLenz <lenzb@oregonstate.edu>
2022-11-09 07:36:28 -0500

    Began players in game refactor

--- src/components/playersInGame.jsx
+++ src/components/playersInGame.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { Component } from 'react';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -8,106 +8,85 @@ import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 
-const columns = [
-  { id: 'name', label: 'Name', minWidth: 80 },
-  {
-    id: 'position',
-    label: 'Position',
-    minWidth: 60,
-    align: 'right',
-  },
-  {
-    id: 'team',
-    label: 'Team',
-    minWidth: 60,
-    align: 'right',
-  },
-  {
-    id: 'points',
-    label: 'Projected Points',
-    minWidth: 60,
-    align: 'right',
-    format: (value) => value.toFixed(2),
-  },
-  {
-    id: 'salary',
-    label: 'Salary',
-    minWidth: 60,
-    align: 'right',
-  },
-];
+class AllPlayersTable extends Component {
+  render() { 
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(25);
+  
+    const handleChangePage = (event, newPage) => {
+      setPage(newPage);
+    };
+  
+    const handleChangeRowsPerPage = (event) => {
+      setRowsPerPage(+event.target.value);
+      setPage(0);
+    };
 
-function createData(name, position, team, points, salary) {
-  return { name, position, team, points, salary };
+    function createData(name, position, team, points, salary) {
+      return { name, position, team, points, salary };
+    }
+
+    const rows = [];
+
+    const columns = [
+      { id: 'name', label: 'Name', minWidth: 80 },
+      { id: 'position', label: 'Position', minWidth: 60, align: 'right' },
+      { id: 'team', label: 'Team', minWidth: 60, align: 'right' },
+      { id: 'points', label: 'Projected Points', minWidth: 60, align: 'right', format: (value) => value.toFixed(2) },
+      { id: 'salary', label: 'Salary', minWidth: 60, align: 'right' },
+    ];
+
+    return (
+      <Paper sx={{ minWidth: '50%', overflow: 'hidden' }}>
+        <TableContainer sx={{ maxHeight: 800 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => {
+                  return (
+                    <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                      {columns.map((column) => {
+                        const value = row[column.id];
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            {column.format && typeof value === 'number'
+                              ? column.format(value)
+                              : value}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[25, 50, 100]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    );
+  }
 }
-
-// Dummy data created with mockarooo
-// https://www.mockaroo.com/
-const rows = [
-
-];
-
-export default function AllPlayersTable() {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(25);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
-
-  return (
-    <Paper sx={{ minWidth: '50%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 800 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[25, 50, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
-  );
-}
+ 
+export default AllPlayersTable;
